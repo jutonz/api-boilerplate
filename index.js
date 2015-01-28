ced73304b68e2050efde42f799c1af4edfff8940
Justin Toniazzo <jutonz42@gmail.com>
2015-01-28 13:56:41 -0500

    Removed bad webscraping stuff

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var Hapi = require('hapi');
 var Good = require('good');
 var request = require('request');
 var weather = require('./lib/weather');
-var naruto = require('./lib/naruto');
 
 var server = new Hapi.Server();
 server.connection({
@@ -35,28 +34,6 @@ server.route({
     var units = 'imperial';
     weather.get(city, units, function(weatherInfo) {
       reply(weatherInfo);
-    });
-  }
-});
-
-server.route({
-  method: 'GET'
-, path: '/naruto-shippuden/episodes/{episodeNumber}'
-, handler: function(request, reply) {
-    var episodeNumber = encodeURIComponent(request.params.episodeNumber);
-    naruto.getInfoForEpisodeNumber(episodeNumber, function(info) {
-      reply(info);
-    });
-  }
-});
-
-server.route({
-  method: 'GET'
-, path: '/naruto-shippuden/seasons/{seasonNumber}'
-, handler: function(request, reply) {
-    var seasonNumber = encodeURIComponent(request.params.seasonNumber);
-    naruto.getInfoForSeasonNumber(seasonNumber, function(info) {
-      reply(info);
     });
   }
 });
